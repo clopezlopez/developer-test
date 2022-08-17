243e65911a6c1ffe4dbd1f5d22bd6325b58a73de
clopezlopez <49844668+clopezlopez@users.noreply.github.com>
2022-08-17 01:09:40 -0700

    styled the form

---
+++ developer-test-main/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./main.css">
+    <title>KHM Travel Developer Test</title>
+</head>
+
+<body>
+    <div class="best-tv-form">
+        <form action="insert-data.php" method="post">
+            <label for="fname" class="mb-1">
+                Best televison show of all time:
+            </label>
+            <input type="text" name="tv-show" class="input-bar mb-1">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</body>
+
+</html>
